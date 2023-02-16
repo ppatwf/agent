54054e4fcdd8572c493fa6b4ca2c7f62010ac869
Ben Moskovitz <ben.m@buildkite.com>
2023-02-16 17:00:43 +1100

    Rename package bootstrap -> job

--- job/integration/job_api_integration_test.go
+++ job/integration/job_api_integration_test.go
@@ -17,9 +17,9 @@ import (
 func TestBootstrapRunsJobAPI(t *testing.T) {
 	defer experimentWithUndo("job-api")()
 
-	tester, err := NewBootstrapTester()
+	tester, err := NewExecutorTester()
 	if err != nil {
-		t.Fatalf("NewBootstrapTester() error = %v", err)
+		t.Fatalf("NewExecutorTester() error = %v", err)
 	}
 	defer tester.Close()
 
